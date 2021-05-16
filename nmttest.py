f104f1f7bf5dc045c4e03a8c3144b125a517c7e2
Gang Zhang <gzhangx@hotmail.com>
2021-05-15 20:57:01 -0400

    test

---
+++ nmttest.py
@@ -0,0 +1,122 @@
+//https://www.tensorflow.org/tutorials/text/nmt_with_attention
+//chcp 936
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+
+
+
+def unicode_to_ascii(s):
+  return ''.join(c for c in unicodedata.normalize('NFD', s)
+                 if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+  w = unicode_to_ascii(w.lower().strip())
+  # creating a space between a word and the punctuation following it
+  # eg: "he is a boy." => "he is a boy ."
+  # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+  w = re.sub(r"([?.!,¿])", r" \1 ", w)
+  w = re.sub(r'[" "]+', " ", w)
+  # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+  w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+  w = w.strip()
+  # adding a start and an end token to the sentence
+  # so that the model know when to start and stop predicting.
+  w = '<start> ' + w + ' <end>'
+  return w
+
+def tokenize(lang):
+  lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+  lang_tokenizer.fit_on_texts(lang)
+  tensor = lang_tokenizer.texts_to_sequences(lang)
+  tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                         padding='post')
+  return tensor, lang_tokenizer
+
+fileContent="你好\tyou are good\n你坏\tyou are bad\n你特别好\tyou are very good\n你特别坏\tyou are very bad"
+def create_dataset(path, num_examples):
+  #lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+  lines = fileContent.strip().split('\n')
+  word_pairs = [[preprocess_sentence(w) for w in line.split('\t')]
+                for line in lines[:num_examples]]
+  return zip(*word_pairs)
+
+def load_dataset(path, num_examples=None):
+  # creating cleaned input, output pairs
+  targ_lang, inp_lang = create_dataset(path, num_examples)
+  input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+  target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+  return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+# Try experimenting with the size of that dataset
+num_examples = 30000
+#input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(path_to_file,num_examples)
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset('no',num_examples)
+
+# Calculate max_length of the target tensors
+max_length_targ, max_length_inp = target_tensor.shape[1], input_tensor.shape[1]
+
+def convert(lang, tensor):
+  for t in tensor:
+    if t != 0:
+      print(f'{t} ----> {lang.index_word[t]}')
+
+
+
+class Encoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+    super(Encoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.enc_units = enc_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    print(f'done init gru ')
+  def call(self, x, hidden):
+    print(f'in call ')
+    print(x)
+    x = self.embedding(x)
+    print(f'after embedding')
+    print(x)
+    output, state = self.gru(x, initial_state=hidden)
+    return output, state
+  def initialize_hidden_state(self):
+    return tf.zeros((self.batch_sz, self.enc_units))
+
+
+BUFFER_SIZE = len(input_tensor)
+BATCH_SIZE = 2
+steps_per_epoch = len(input_tensor)//BATCH_SIZE
+embedding_dim = 3  ### originally 256
+units = 1024
+vocab_inp_size = len(inp_lang.word_index)+1
+vocab_tar_size = len(targ_lang.word_index)+1
+
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor, target_tensor)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+
+example_input_batch, example_target_batch = next(iter(dataset))
+
+sample_hidden = encoder.initialize_hidden_state()
+
+encoder = Encoder(vocab_inp_size, embedding_dim, units, 2)
+sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+print('Encoder output shape: (batch size, sequence length, units)', sample_output.shape)
+print('Encoder Hidden state shape: (batch size, units)', sample_hidden.shape)
+
+
